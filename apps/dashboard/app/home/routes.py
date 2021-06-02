2df1ccef150d0c52da8644109736a6334975dcc1
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2021-06-02 21:17:16 +0700

    new dashboard

--- apps/dashboard/app/home/routes.py
+++ apps/dashboard/app/home/routes.py
@@ -7,7 +7,7 @@ from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
 
-BASE_URL = 'http://192.168.1.21:8000/'
+BASE_URL = 'https://lymhp-tf.herokuapp.com/'
 
 @blueprint.route('/index')
 @login_required
